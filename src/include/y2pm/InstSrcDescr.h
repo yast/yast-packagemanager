91e13a0c13e5db6bd30f50b9cea18ece373edc4c
Ludwig Nussel <lnussel@suse.de>
2002-06-25 13:53:29 +0000

    added some methods
    svn path=/trunk/packagemanager/; revision=2881

--- src/include/y2pm/InstSrcDescr.h
+++ src/include/y2pm/InstSrcDescr.h
@@ -21,6 +21,8 @@
 
 #include <iosfwd>
 
+#include <string>
+#include <list>
 #include <y2pm/InstSrcDescrPtr.h>
 
 ///////////////////////////////////////////////////////////////////
@@ -37,8 +39,57 @@ class REP_CLASS(InstSrcDescr) {
     InstSrcDescr();
 
     virtual ~InstSrcDescr();
+  
+  private:
+    std::string _vendor;
+    std::string _default_language;
+    std::string _default_timezone;
+    std::string _version;
+    int _release;
+    std::string _name;
+    std::string _product;
+    std::string _id;
+    std::string _type;
+    int _numparts;
+    std::list<std::string> _architectures;
 
   public:
+    /** SuSE */
+    const std::string& Vendor() { return _vendor; }
+    /** de */
+    const std::string& Language() { return _default_language; }
+    /** Europe/Berlin */
+    const std::string& Timezone() { return _default_timezone; }
+    /** 8.1 */
+    const std::string& Version() { return _version; }
+    /** 0 */
+    int Release() { return _release; }
+    /** SuSE Linux */
+    const std::string& Name() { return _name; }
+    /** Professional, SLES */
+    const std::string& Product() { return _product; }
+    /** id to identify medium */
+    const std::string& Id() { return _id; }
+    /** Full, Update/Patch, Maintenance, Customer, ... */
+    const std::string& Type() { return _type; }
+    /** eg number cds */
+    int NumParts() { return _numparts; }
+    /** i386, ppc, noarch */
+    const std::list<std::string>& Architectures() { return _architectures; }
+
+    void setVendor( const std::string& vendor) { _vendor = vendor; }
+    void setLanguage( const std::string& language) { _default_language = language; }
+    void setTimezone( const std::string& timezone) { _default_timezone = timezone; }
+    void setVersion( const std::string& version) { _version = version; }
+    void setRelease( int release ) { _release = release; }
+    void setName( const std::string& name) { _name = name; }
+    void setProduct( const std::string& product) { _product = product; }
+    void setId( const std::string& id) { _id = id; }
+    void setType( const std::string& type) { _type = type; }
+    void setNumParts( int num ) { _numparts = num; }
+    void setArchitectures( const std::list<std::string>& archs) { _architectures = archs; }
+
+    bool operator==(const InstSrcDescr& d) {return (_id == d._id); }
 
     virtual std::ostream & dumpOn( std::ostream & str ) const;
 };
