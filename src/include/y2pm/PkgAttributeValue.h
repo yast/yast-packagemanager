d79787ea7423e520732f3199a367e94ecdf2d000
Michael Andres <ma@suse.de>
2002-07-29 15:46:17 +0000

    Transport class designed to contain string values returned by generic Object attribute queries via PMObject::getAttributeValue.
    svn path=/trunk/packagemanager/; revision=4033

---
+++ src/include/y2pm/PkgAttributeValue.h
@@ -0,0 +1,75 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
+
+  File:       PkgAttributeValue.h
+
+  Author:     Michael Andres <ma@suse.de>
+  Maintainer: Michael Andres <ma@suse.de>
+
+  Purpose: Kludge to return Object attributes. Dependent on the kind
+  of attribute you may either expect a single string value (NAME, VERSION, etc.)
+  or a list of strings (dependencies, filelist, etc).
+
+/-*/
+#ifndef PkgAttributeValue_h
+#define PkgAttributeValue_h
+
+#include <iosfwd>
+#include <list>
+#include <string>
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : PkgAttributeValue
+/**
+ * Kludge to return Object attributes. Dependent on the kind of attribute
+ * you may either expect a single string value (NAME, VERSION, etc.) or
+ * a list of strings (dependencies, filelist, etc).
+ *
+ * While it's basically a std::list<std::string>, we'll add some
+ * convenience functions mainly for the UI.
+ **/
+class PkgAttributeValue : public std::list<std::string> {
+
+  public:
+
+    /**
+     * Construct as empty list
+     **/
+    PkgAttributeValue();
+
+    /**
+     * Construct als list containing one entry val_r only
+     **/
+    PkgAttributeValue( const std::string & val_r );
+
+    /**
+     * Construct als copy of list val_r
+     **/
+    PkgAttributeValue( const std::list<std::string> & val_r );
+
+    ~PkgAttributeValue();
+
+  public:
+
+    /**
+     * Usefull for single string values if you don't mind whether there's
+     * actually a value stored inside (size==1). It returns the default_r
+     * (per default empty), if the list is empty. Ohterwise the first
+     * (and probabely only) string stored..
+     **/
+    std::string firstLine( const std::string & default_r = "" ) const;
+};
+
+///////////////////////////////////////////////////////////////////
+
+#endif // PkgAttributeValue_h
