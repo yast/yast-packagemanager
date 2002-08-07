226ad9f0674a8d889b38ff978eea230ca105d5e2
Ludwig Nussel <lnussel@suse.de>
2002-08-07 12:45:25 +0000

    the actual InstTargetError class ...
    svn path=/trunk/packagemanager/; revision=4311

---
+++ src/inst/include/y2pm/InstTargetError.h
@@ -0,0 +1,69 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                     (C) 2002 SuSE AG |
+\----------------------------------------------------------------------/
+
+  File:       InstTargetError.h
+
+  Author:     Ludwig Nussel <lnussel@suse.de>
+  Maintainer: Ludwig Nussel <lnussel@suse.de>
+
+  Purpose: Definition of "InstTarget" error values.
+
+
+/-*/
+#ifndef InstTargetError_h
+#define InstTargetError_h
+
+#include <iosfwd>
+
+#include <y2pm/PMError.h>
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : InstTargetError
+/**
+ * @short Definition of "InstTarget" error values.
+ * @see PMError
+ **/
+class InstTargetError {
+
+  private:
+
+    friend class PMError;
+
+    static const std::string errclass;
+
+    static std::string errtext( const unsigned e );
+
+  public:
+
+    enum Error {
+      E_ok    = PMError::E_ok,        	// no error
+      E_error = PMError::C_InstTargetError,	// some error
+      // more specific errors start here:
+      E_system,		// see errno
+      E_RpmDB_not_found,
+      E_RpmDB_create_failed,
+      E_RpmDB_check_old_version_failed,
+      E_RpmDB_mkdir_failed,
+      E_RpmDB_initdb_failed,
+      E_RpmDB_copy_tmpdb_failed,
+      E_RpmDB_rebuilddb_failed,
+      E_RpmDB_not_initialized,
+      E_RpmDB_subprocess_failed,
+
+    };
+};
+
+///////////////////////////////////////////////////////////////////
+
+#endif // InstTargetError_h
+
