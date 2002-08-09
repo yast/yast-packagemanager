aadce24e5c13b9fb58050a1d79553db8ce3a40df
Cornelius Schumacher <cschum@suse.de>
2002-08-09 15:38:35 +0000

    Added functions to retrieve all patches from directory on server. Added class which holds the path information for the patches.
    svn path=/trunk/packagemanager/; revision=4417

--- src/you/include/y2pm/PMYouPatchInfo.h
+++ src/you/include/y2pm/PMYouPatchInfo.h
@@ -25,6 +25,8 @@
 #include <string>
 
 #include <y2util/CommonPkdParser.h>
+#include <y2util/Url.h>
+#include <y2util/Pathname.h>
 
 #include <y2pm/PMError.h>
 
@@ -97,9 +99,37 @@ class YOUPatchTagSet : public CommonPkdParser::TagSet
 
 ///////////////////////////////////////////////////////////////////
 //
+//	CLASS NAME : PMYouPatchPaths
+/**
+ * Pathes for patches.
+ **/
+class PMYouPatchPaths {
+
+  public:
+    PMYouPatchPaths( const std::string &product, const std::string &version,
+                     const std::string &arch );
+
+    void setPatchPath( const Pathname & );
+    Pathname patchPath();
+
+    void setPatchUrl( const Url & );
+    Url patchUrl();
+
+    void setAttachPoint( const Pathname & );
+    Pathname attachPoint();
+
+  private:
+    Pathname _patchPath;
+    Url _patchUrl;
+    Pathname _attachPoint;
+};
+
+
+///////////////////////////////////////////////////////////////////
+//
 //	CLASS NAME : PMYouPatchInfo
 /**
- * The Package.
+ * Patch information.
  **/
 class PMYouPatchInfo {
 
@@ -112,19 +142,47 @@ class PMYouPatchInfo {
     PMYouPatchInfo( const std::string &lang );
 
     /**
-     * Read patch info from file.
+     * Destructor
+     **/
+    ~PMYouPatchInfo();
+
+    /**
+     * Get patches from specified location.
      *
-     * @param path    File path of package info file.
+     * @param paths   Object holding the path location information.
      * @param patches List of patch objects where the results are stored.
      **/
-    PMError readFile( const std::string &path, const std::string &name,
+    PMError getPatches( PMYouPatchPaths *paths,
+                        std::list<PMYouPatchPtr> &patches );
+    
+    /**
+     * Read patch info files from directory.
+     *
+     * @param baseUrl    Base of URL where patches are located.
+     * @param patchPath  Path of patch directory relative to baseUrl.
+     * @param attachPath Path where media is to be attached.
+     * @param patches    List of patch objects where the results are stored.
+     **/
+    PMError readDir( const Url &baseUrl, const Pathname &patchPath,
+                     const Pathname &attachPath,
+                     std::list<PMYouPatchPtr> &patches );
+
+    /**
+     * Read patch info from file.
+     *
+     * @param path     File path of package info file.
+     * @param fileName Name of patch file.
+     * @param patches  List of patch objects where the results are stored.
+     **/
+    PMError readFile( const Pathname &path, const std::string &fileName,
                       std::list<PMYouPatchPtr> &patches );
-  
+
   protected:
     std::string tagValue( YOUPatchTagSet::Tags tag );
 
   private:
-    CommonPkdParser::TagSet* _tagset;
+    CommonPkdParser::TagSet *_tagset;
+    std::list<std::string> *_patchFiles;
 };
 
 ///////////////////////////////////////////////////////////////////
