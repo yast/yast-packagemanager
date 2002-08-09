e3f764495e448cfa59eb83b113556f8a20b6ccb3
Michael Andres <ma@suse.de>
2002-08-09 16:41:16 +0000

    PkgEdition's usage of char* leads to SEGVs on comparison. Changed class to use strings.
    svn path=/trunk/packagemanager/; revision=4430

--- src/include/y2pm/PkgEdition.h
+++ src/include/y2pm/PkgEdition.h
@@ -7,46 +7,50 @@
 enum rel_op { NONE, EQ, NE, LT, LE, GT, GE };
 extern const char* op_str[];
 
-// PkgEdition represents version, release, and epoch of a package.
-// It provides methods for comparing.
-// It can also represent the special editions MAXIMUM (greater than all real
-// editions) and UNSPEC (uncomparable).
+/**
+ * @short PkgEdition represents  [epoch:]version-release
+ *
+ * PkgEdition represents version, release, and epoch of a package.
+ * It provides methods for comparing.
+ * It can also represent the special editions MAXIMUM (greater than all real
+ * editions) and UNSPEC (uncomparable).
+ **/
 class PkgEdition {
+
   public:
-	enum type_enum { NORMAL, EPOCH, MAXIMUM, UNSPEC };
+
+    enum type_enum { NORMAL, EPOCH, MAXIMUM, UNSPEC };
 
   private:
 
         // Use a 'version-release' form for these strings.
         // (i.e. exactly one '-')
-	static const char *const _str_UNSPEC  = "EDITION-UNSPEC";
-	static const char *const _str_MAXIMUM = "EDITION-MAXIMUM";
+	static const std::string _str_UNSPEC;
+	static const std::string _str_MAXIMUM;
 
 	type_enum type;
 	int _epoch;
 	int _buildtime;
 	int _metahash;
-	char *_version;
-	char *_release;
+	std::string _version;
+	std::string _release;
 
 	bool edition_eq( const PkgEdition& e2 ) const;
 	bool edition_lt( const PkgEdition& e2 ) const;
-	int rpmvercmp(const char * a, const char * b) const;
+
+	int rpmvercmp( const std::string & a, const std::string & b ) const;
 
 	// helper for copy constructor and operator=
 	void xconstruct( type_enum xtype, int buildtime, int metahash,
-		int epoch, const char *v, const char *r
-	) {
+			 int epoch, const char *v, const char *r ) {
 		type = xtype;
 		_epoch = epoch;
 		_buildtime = buildtime;
 		_metahash = metahash;
-		_version = v ? strdup(v) : NULL;
-		_release = r ? strdup(r) : NULL;
+		_version = v ? v : "";
+		_release = r ? r : "";
 	}
-	void yconstruct( const PkgEdition& e ) {
-		xconstruct(e.type,e._buildtime,e._metahash,e._epoch,e._version,e._release);
-	}
+
   public:
 	PkgEdition( const char *v = "", const char *r = NULL ) {
 		xconstruct(NORMAL,0,0,0,v,r);
@@ -64,54 +68,24 @@ class PkgEdition {
 		assert( t == MAXIMUM || t == UNSPEC );
 		xconstruct(t,0,0,0,NULL,NULL);
 	}
-	PkgEdition( const PkgEdition& e ) {
-		yconstruct(e);
-	}
-	~PkgEdition() {
-		if (type == MAXIMUM || type == UNSPEC)
-			return;
-		if (_version) {
-			free( _version );
-			_version = NULL;
-		}
-		if (_release) {
-			free( _release );
-			_release = NULL;
-		}
-	}
-
-	PkgEdition operator= (const PkgEdition& e) {
-		if (type == NORMAL || type == EPOCH) {
-			if (_version) {
-				free( _version );
-				_version = NULL;
-			}
-			if (_release) {
-				free( _release );
-				_release = NULL;
-			}
-		}
-		yconstruct( e );
-		return *this;
-	}
+	~PkgEdition() {}
 
 	const char *version() const {
 		switch( type ) {
 		  case NORMAL:
 		  case EPOCH:
-			return _version;
+			return _version.c_str();
 		  case MAXIMUM:
-			return _str_MAXIMUM;
+			return _str_MAXIMUM.c_str();
 		  case UNSPEC:
-			return _str_UNSPEC;
-		  default:
-			return "";
+			return _str_UNSPEC.c_str();
 		}
+		return "";
 	}
 	const char *release() const {
-		return (type == NORMAL || type == EPOCH) && _release ? _release : "";
+		return (type == NORMAL || type == EPOCH) ? _release.c_str() : "";
 	}
-	bool has_release() const { return _release != NULL; }
+
 	int epoch() const { return type == EPOCH ? _epoch : 0; }
 
 	int buildtime() const { return type == NORMAL ? _buildtime : 0; }
@@ -130,7 +104,7 @@ class PkgEdition {
 	bool operator>=( const PkgEdition& e2 ) const { return compare( GE, e2 ); }
 
 	std::string as_string() const;
-	operator std::string() const { return as_string(); }
+	//operator std::string() const { return as_string(); }
 
 	friend std::ostream& operator<<( std::ostream&, const PkgEdition& );
 
