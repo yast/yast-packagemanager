a4f918b4e86bce71c4f550612e4d24e45b08aa22
Gabriele Mohr <gs@suse.de>
2002-08-09 16:19:16 +0000

    class containing the tag sets for the TagParser
    svn path=/trunk/packagemanager/; revision=4423

---
+++ src/inst/include/y2pm/InstSrcTags.h
@@ -0,0 +1,74 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
+
+  File:       InstSrcTags.h
+
+  Author:     Gabriele Strattner <gs@suse.de>
+  Maintainer: Michael Andres <ma@suse.de>
+
+  Purpose:	Class containing tags, e.g. used in media cache file.
+                The tag sets are needed for the TagParser. 
+/-*/
+
+#include <fstream>
+#include <y2util/CommonPkdParser.h>
+
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : InstSrcMediaTags
+/**
+ * @short provides the tag set for the media file
+ * (to feed the tag parser) 
+ *
+ **/
+class InstSrcMediaTags : public CommonPkdParser::TagSet
+{
+
+public:
+    enum Tags {
+	ARCH,		// architecture
+	TYPE,		// type found on medium
+	URL,
+	PRODUCTDIR,	// product dir below _url
+	ACTIVATE,	// 1 = true (default activated), 0 = false
+	MEDIA,		// media_vendor, media_id, media_count
+	NUM_TAGS
+    };
+
+public:
+    InstSrcMediaTags( ) 
+	: TagSet()	{
+
+	CommonPkdParser::Tag* t;
+	createTag( "=Arch", ARCH );
+	createTag( "=Type", TYPE );
+	createTag( "=URL", URL );
+	createTag( "=Productdir", PRODUCTDIR );
+	createTag( "=Default_activate", ACTIVATE );
+	t = createTag( "+Media", MEDIA );
+	t->setEndTag("-Media");
+    };
+
+private:
+
+    CommonPkdParser::Tag* createTag( std::string tagName, Tags tagEnum ) {
+	
+	CommonPkdParser::Tag* t;
+	t = new CommonPkdParser::Tag( tagName, CommonPkdParser::Tag::ACCEPTONCE );
+	this->addTag(t);
+	addTagByIndex( tagEnum, t );
+
+	return t;
+    }
+    
+};
