37108190b13ba77afb48b791601a95ca89fb8d8f
Cornelius Schumacher <cschum@suse.de>
2002-08-15 11:05:38 +0000

    Moved PMYouPatchPaths class to own file.
    svn path=/trunk/packagemanager/; revision=4746

---
+++ src/you/PMYouPatchPaths.cc
@@ -0,0 +1,180 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                    (C) SuSE Linux AG |
+\----------------------------------------------------------------------/
+
+  File:       PMYouPatchPaths.cc
+
+  Author:     Cornelius Schumacher <cschum@suse.de>
+  Maintainer: Cornelius Schumacher <cschum@suse.de>
+
+  Purpose: Provide path information for YOU patches.
+
+/-*/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <y2util/Y2SLog.h>
+
+#include <y2pm/InstSrcError.h>
+#include <y2pm/MediaAccess.h>
+#include <y2pm/PMPackage.h>
+
+#include <y2pm/PMYouPatchPaths.h>
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : PMYouPatchPaths
+//
+///////////////////////////////////////////////////////////////////
+
+PMYouPatchPaths::PMYouPatchPaths( const string &product, const string &version,
+                                  const string &arch )
+  : _arch( arch )
+{
+  _businessProduct = ( product != "SuSE-Linux" );
+
+  string path = arch + "/update/";
+  if ( _businessProduct  ) {
+    path += product + "/";
+    _patchUrl = Url( "http://support.suse.de/" );
+  } else {
+    _patchUrl = Url( "ftp://ftp.suse.com/pub/suse/" );
+  }
+  path += version;
+  
+  _patchPath = path + "/patches/";
+  _rpmPath = path + "/rpm/";
+}
+
+void PMYouPatchPaths::setPatchPath( const Pathname &path )
+{
+  _patchPath = path;
+}
+
+Pathname PMYouPatchPaths::patchPath()
+{
+  return _patchPath;
+}
+
+Pathname PMYouPatchPaths::rpmPath( const PMPackagePtr &pkg )
+{
+  string rpmName = pkg->name();
+  rpmName += "-";
+  rpmName += pkg->version();
+  rpmName += "-";
+  rpmName += pkg->release();
+  rpmName += ".";
+  rpmName += arch();
+  rpmName += ".rpm";
+  return _rpmPath + rpmName;
+}
+
+void PMYouPatchPaths::setPatchUrl( const Url &url )
+{
+  _patchUrl = url;
+}
+
+Url PMYouPatchPaths::patchUrl()
+{
+  return _patchUrl;
+}
+
+Pathname PMYouPatchPaths::localDir()
+{
+  return "/var/lib/YaST2/you/";
+}
+
+PkgArch PMYouPatchPaths::arch()
+{
+  return _arch;
+}
+
+bool PMYouPatchPaths::businessProduct()
+{
+  return _businessProduct;
+}
+
+PMError PMYouPatchPaths::requestServers( const string &url, const string &file )
+{
+  D__ << "url: '" << url << "' file: '" << file << "'" << endl;
+
+  Url u;
+  if ( url.empty() ) {
+    u = Url( "http://www.suse.de/de/support/download/" );
+  } else {
+    u = url;
+    if ( !u.isValid() ) return InstSrcError::E_bad_url;
+  }
+
+  Pathname f;
+  if ( file.empty() ) {
+    if ( businessProduct() ) {
+      f = "suseservers_http.txt";
+    } else {
+      f = "suseservers.txt";
+    }
+  } else {
+    f = file;
+  }
+
+  D__ << "url: '" << u << "' file: '" << f.asString() << "'" << endl;
+
+  MediaAccess media;
+
+  PMError error = media.open( u );
+  if ( error ) return error;
+
+  error = media.attach();
+  if ( error ) return error;
+
+  error = media.provideFile( f );
+  if ( error ) return error;
+
+  string line;
+  ifstream in( ( media.localPath( f ) ).asString().c_str() );
+  while( getline( in, line ) ) {
+    if ( *line.begin() != '#' ) {
+      Url url( line );
+      if ( url.isValid() ) _servers.push_back( url );
+      D__ << "Mirror url: " << url.asString() << endl;
+    }
+  }
+
+  return PMError();
+}
+
+list<Url> PMYouPatchPaths::servers()
+{
+  if ( _servers.size() == 0 ) {
+    list<Url> servers;
+    servers.push_back( defaultServer() );
+    return servers;
+  } else {
+    return _servers;
+  }
+}
+
+Url PMYouPatchPaths::defaultServer()
+{
+  if ( _servers.size() == 0 ) {
+    if ( businessProduct() ) {
+      return Url( "http://sdb.suse.de/download/" );
+    } else {
+      return Url( "ftp://ftp.suse.com/pub/suse/" );
+    }
+  } else {
+    return *_servers.begin();
+  }
+}
---
+++ src/you/include/y2pm/PMYouPatchPaths.h
@@ -0,0 +1,81 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                    (C) SuSE Linux AG |
+\----------------------------------------------------------------------/
+
+  File:       PMYouPatchPaths.h
+
+  Author:     Cornelius Schumacher <cschum@suse.de>
+  Maintainer: Cornelius Schumacher <cschum@suse.de>
+
+  Purpose: Class providing path information for YOU patches.
+
+/-*/
+#ifndef PMYouPatchPaths_h
+#define PMYouPatchPaths_h
+
+#include <list>
+#include <string>
+
+#include <y2util/Url.h>
+#include <y2util/Pathname.h>
+
+#include <y2pm/PMError.h>
+
+#include <y2pm/PMPackagePtr.h>
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : PMYouPatchPaths
+/**
+ * Pathes for patches.
+ **/
+class PMYouPatchPaths {
+
+  public:
+    PMYouPatchPaths( const std::string &product, const std::string &version,
+                     const std::string &arch );
+
+    void setPatchPath( const Pathname & );
+    Pathname patchPath();
+
+    Pathname rpmPath( const PMPackagePtr &pkg );
+
+    void setPatchUrl( const Url & );
+    Url patchUrl();
+
+    Pathname localDir();
+
+    PkgArch arch();
+
+    bool businessProduct();
+
+    PMError requestServers( const std::string &url = "",
+                            const std::string &file = "" );
+
+    std::list<Url> servers();
+    
+    Url defaultServer();
+
+  private:
+    Pathname _patchPath;
+    Pathname _rpmPath;
+    Url _patchUrl;
+    
+    PkgArch _arch;
+    
+    std::list<Url> _servers;
+
+    bool _businessProduct;
+};
+
+///////////////////////////////////////////////////////////////////
+
+#endif // PMYouPatchPaths_h
