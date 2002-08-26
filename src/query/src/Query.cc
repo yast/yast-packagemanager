90212097a5886bf4910ba234a5bfca3ab6215fc1
Klaus Kämpf <kkaempf@suse.de>
2002-08-26 07:01:33 +0000

    fix to compile (still WIP)
    svn path=/trunk/packagemanager/; revision=5264

--- src/query/src/Query.cc
+++ src/query/src/Query.cc
@@ -21,9 +21,17 @@
 
 #include <y2util/Y2SLog.h>
 
+#include <y2pm/PMPackage.h>
+#include <y2pm/PMSelectable.h>
+
 #include <y2pm/Query.h>
-#include <y2pm/PMManager.h> 
+
 using namespace std;
+
+#define MASK_NONE	((Query::BitMask)0x00)
+#define MASK_INSTALLED	((Query::BitMask)0x01)
+#define MASK_AVAILABLE	((Query::BitMask)~0x01)
+#define FIRST_AVAILABLE ((Query::BitMask)0x02)
 
 ///////////////////////////////////////////////////////////////////
 //
@@ -38,21 +46,201 @@ IMPL_BASE_POINTER( Query );
 // private
 
 bool
-Query::checkSelectable (PMSelectablePtr selectable)
+Query::checkPackage (PMPackagePtr package, struct qnode *query)
 {
     return false;
 }
 
-bool
-Query::checkPackage (PMPackagePtr package)
+Query::BitMask
+Query::findCandidate (PMSelectablePtr selectable)
 {
-    return false;
+    Query::BitMask ret = MASK_NONE;
+
+    if (!selectable->has_candidate())
+	return ret;
+
+    Query::BitMask current = FIRST_AVAILABLE;
+    PMPackagePtr candidate = selectable->candidateObj();
+    for (PMSelectable::PMObjectList::const_iterator it = selectable->av_begin();
+	 it != selectable->av_end(); ++it)
+    {
+	if (candidate == *it)
+	{
+	    ret = current;
+	    break;
+	}
+	current <<= 1;
+    }
+    return ret;
 }
 
-bool
-Query::checkSelection (PMSelectionPtr selection)
+Query::BitMask
+Query::checkSelectableFlag (PMSelectablePtr selectable, int flag)
 {
-    return false;
+    MIL << "Query::checkSelectableFlag (" << flag << endl;
+
+    Query::BitMask ret = MASK_NONE;
+    switch (flag)
+    {
+	case 1:			//    { "isInstalled",	1 },
+	{
+	    if (selectable->installedObj())
+	    {
+		ret = MASK_INSTALLED;
+	    }
+	}
+	break;
+	case 2:			//    { "isAvailable",	2 },
+	{
+	    Query::BitMask current = FIRST_AVAILABLE;
+	    for (PMSelectable::PMObjectList::const_iterator it = selectable->av_begin();
+		 it != selectable->av_end(); ++it)
+	    {
+		ret |= current;
+		current <<= 1;
+	    }
+	}
+	break;
+	case 3:			//    { "isSelected",	3 },
+	{
+	    if (selectable->to_install())
+	    {
+		ret = findCandidate (selectable);
+	    }
+	}
+	case 4:			//    { "isDeleted",	4 },
+	{
+	    if (selectable->to_delete()
+		&& selectable->has_installed())
+	    {
+		ret = MASK_INSTALLED;
+	    }
+	}
+	case 5:			//    { "isUpdated",	5 },
+	{
+	    if (selectable->to_install()
+		&& selectable->has_installed()
+		&& selectable->has_candidate())
+	    {
+		ret = findCandidate (selectable);
+	    }
+	}
+	case 6:			//    { "isTaboo",	6 },
+	{
+	    if (selectable->is_taboo()
+		&& (selectable->has_candidate()))
+	    {
+		ret = findCandidate (selectable);
+	    }
+	}
+	break;
+	case 7:			//    { "isAutomatic",	7 },
+	{
+	    if (selectable->status() == PMSelectable::S_Auto
+		&& selectable->has_candidate())
+	    {
+		ret = findCandidate (selectable);
+	    }
+	}
+	case 8:			//    { "isIncomplete",	8 },
+	break;
+	default:
+	break;
+    }
+    MIL << "ret " << ret << endl;
+    return ret;
+}
+
+
+Query::BitMask
+Query::checkSelectableOpCompare (PMSelectablePtr selectable, struct qnode *query)
+{
+    Query::BitMask ret = MASK_NONE;
+
+    return ret;
+}
+
+Query::BitMask
+Query::checkSelectableOpBoolean (PMSelectablePtr selectable, struct qnode *query)
+{
+    Query::BitMask left = checkSelectableValue (selectable, &(query->left));
+
+    if ((query->op == OP_AND)
+	&& (left == MASK_NONE))
+    {
+	return left;
+    }
+
+    Query::BitMask right = checkSelectableValue (selectable, &(query->right));
+    if (query->op == OP_AND)
+    {
+	return left & right;
+    }
+    if (query->op == OP_OR)
+    {
+	return left | right;
+    }
+    ERR << "Query::checkSelectableOpBoolean non boolean " << query->op << endl;
+    return MASK_NONE;
+}
+
+
+Query::BitMask
+Query::checkSelectableValue (PMSelectablePtr selectable, struct qvalue *value)
+{
+    MIL << "Query::checkSelectableValue ()" << endl;
+    Query::BitMask ret = MASK_NONE;
+    switch (value->type)
+    {
+	case QTYPE_FLAG:
+	    ret = checkSelectableFlag (selectable, value->v.attr);
+	break;
+	case QTYPE_ATTR:
+	    MIL << "QTYPE_ATTR ?!" << endl;
+	break;
+	case QTYPE_CONST:
+	    MIL << "QTYPE_CONST ?!" << endl;
+	break;
+	case QTYPE_NODE:
+	    ret = checkSelectable (selectable, value->v.node);
+	break;
+	case QTYPE_NONE:
+	    MIL << "QTYPE_NONE ?!" << endl;
+	break;
+    }
+    MIL << "ret " << ret << endl;
+    return ret;
+}
+
+
+Query::BitMask
+Query::checkSelectable (PMSelectablePtr selectable, struct qnode *query)
+{
+    Query::BitMask ret = MASK_NONE;
+    if (query->op > OP_BOOLEAN)
+    {
+	ret = checkSelectableOpCompare (selectable, query);
+    }
+    else if (query->op > OP_UNARY)
+    {
+	ret = checkSelectableOpBoolean (selectable, query);
+    }
+    else if (query->op == OP_NOT)
+    {
+	ret = ~checkSelectableValue (selectable, &(query->left));
+    }
+    else if (query->op == OP_FLAG)
+    {
+	if (query->left.type != QTYPE_ATTR)
+	{
+	    ERR << "op flag but type " << query->left.type << " != attr" << endl;
+	}
+	else
+	{
+	    ret = checkSelectableFlag (selectable, query->left.v.attr);
+	}
+    }
+    return ret;
 }
 
 //-----------------------------------------------------------------
@@ -69,6 +257,7 @@ Query::checkSelection (PMSelectionPtr selection)
 //
 Query::Query()
     : _query (0)
+    , _initial (true)
 {
 }
 
@@ -95,35 +284,99 @@ Query::~Query()
 //	DESCRIPTION : parse textual query to filter
 //
 PMError
-Query::parseQuery (const std::string& query)
+Query::queryPackage (const std::string& query)
 {
     QueryParser parser;
-    return parser.parseQuery (query, _errpos, &_query);
-}
-
-
-const std::list<PMSelectablePtr>
-Query::querySelectables (const PMManager& packageManager)
-{
-    std::list<PMSelectablePtr> matches;
-    for (PMManager::PMSelectableVec::const_iterator it = packageManager.begin();
-	 it != packageManager.end(); ++it)
+    PMError err = parser.parseQuery (query, _errpos, &_query);
+    if (err == PMError::E_ok)
     {
-	if (checkSelectable (*it))
-	    matches.push_back (*it);
+	_initial = true;
+	_backlog = MASK_NONE;
     }
-    return matches;
+    else
+    {
+	_query = 0;
+	_backlog = MASK_NONE;
+    }
+    return err;
 }
 
-const std::list<PMPackagePtr>
-Query::queryPackages (const PMManager& packageManager)
-{
-    return std::list<PMPackagePtr>();
-}
 
-const std::list<PMSelectionPtr>
-Query::querySelections (const PMManager& selectionManager)
+const PMPackagePtr
+Query::nextPackage (const PMPackageManager& packageManager)
 {
-    return std::list<PMSelectionPtr>();
+    PMPackagePtr package;
+
+    // no query -> exit
+    if (_query == 0)
+    {
+	return package;
+    }
+
+    // first call to nextPackage(), initialize
+    if (_initial)
+    {
+	_current = packageManager.begin();
+	_initial = false;
+    }
+
+    // no bits in backlog -> check selectables
+    if (_backlog == MASK_NONE)
+    {
+	while (_current != packageManager.end())
+	{
+	    _backlog = checkSelectable (*_current, _query);
+
+	    if (_backlog != MASK_NONE)			// Hit !
+	    {
+		++_current;
+		_logpos = MASK_INSTALLED;
+		break;
+	    }
+	}
+    }
+
+    // anything found ?
+
+    if (_backlog != MASK_NONE)
+    {
+	// find next bit
+
+	while ((_logpos != MASK_NONE)
+		&& ((_backlog & _logpos) == MASK_NONE))
+	{
+	    _logpos <<= 1;
+	}
+
+	// no more found but _backlog still set ?!
+
+	if (_logpos == MASK_NONE)
+	{
+	    ERR << "_backlog " << _backlog << " not found" << endl;
+	}
+	else if (_logpos == MASK_INSTALLED)
+	{
+	    // return installed
+	    package = (*_current)->installedObj();
+	    _backlog &= ~_logpos;
+	}
+	else
+	{
+	    // an available
+	    Query::BitMask av_mask = FIRST_AVAILABLE;
+	    for (PMSelectable::PMObjectList::const_iterator it = (*_current)->av_begin();
+		 it != (*_current)->av_end(); ++it)
+	    {
+		if (_backlog & av_mask)
+		{
+		    _logpos = av_mask;
+		    _backlog &= ~_logpos;
+		    package = *it;
+		    break;
+		}
+	   }
+	}
+    }
+    return package;
 }
 
