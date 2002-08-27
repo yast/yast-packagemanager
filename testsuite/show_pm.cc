d6274b50258823f5d51c54403c4f992ca3a94fe1
Klaus Kämpf <kkaempf@suse.de>
2002-08-27 08:48:13 +0000

    show also the empty locale
    svn path=/trunk/packagemanager/; revision=5358

--- testsuite/show_pm.cc
+++ testsuite/show_pm.cc
@@ -159,7 +159,7 @@ show_pmselection (PMSelectionPtr s)
     s->startRetrieval();
     show_pmobject ((PMObjectPtr)s);
 
-    char *locales[] = { "de", "en", "cs", "es", "fr", "gl", "hu", "it", "ja", "nl", "pt", "sv", "tr", "ru", "cz", 0 };
+    char *locales[] = { "", "de", "en", "cs", "es", "fr", "gl", "hu", "it", "ja", "nl", "pt", "sv", "tr", "ru", "cz", 0 };
     int pos = 0;
     cout << "Category: " << s->category () << endl;
     cout << "IsBase: " << s->isBase () << endl;
