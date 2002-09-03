e86c9e2ba9a6642ac0330584ff1de7c6cd51c453
Ludwig Nussel <lnussel@suse.de>
2002-09-03 12:05:21 +0000

    - copied tex-docu for PkgSet into header - process rpm warnings about config files after rpm has finished as rpm seems   to generate the warning before extracting the archive - made testuite programs compile
    svn path=/trunk/packagemanager/; revision=5865

--- src/include/y2pm/PkgSet.h
+++ src/include/y2pm/PkgSet.h
@@ -10,6 +10,19 @@ inline size_t hashfun( const PkgName & ustr_r ) {
     return hashfun( (const Ustring &)ustr_r );
 }
 
+/** A PkgSet represents an unordered collection of packages. This
+may be e.g. a set of currently installed packages, to be installed packages, or
+ones that are available for installation. A package name must be unique in a
+PkgSet, i.e. it is not possible to include more than one version of a package
+
+<p>
+
+Additionally to compounding packages, has one more important feature for
+dependency checking: It maintains reverse indices of all package relations.
+I.e., you can ask the set what packages (e.g.) require or provide a certain
+name. These reverse indices are the core of an efficient dependency and
+consistency checking.
+ * */
 
 class PkgSet {
   public:
@@ -52,26 +65,33 @@ class PkgSet {
 	//
 	bool empty() { return contents.empty(); }
 
-	// add a single package
+	/** Adds a package to the set. If force is false, the package is added
+	 * only if no equal or newer edition is already member of the set.
+	 * This is handy most of the time, but can be overridden by passing
+	 * true as second parameter. Then pkg is always added, replacing a
+	 * previous member of the set with the same name.  */
 	void add( PMSolvablePtr pkg, bool force = false );
-	// remove a package
-	void remove( PMSolvablePtr pkg );
-	void remove( PkgName name ) {
-		PMSolvablePtr pkg = lookup(name);
-		if (pkg)
-			remove( pkg );
-	}
+	/** remove a package */
+	void remove( PMSolvablePtr pkg ); void remove( PkgName name ) {
+		PMSolvablePtr pkg = lookup(name); if (pkg) remove( pkg ); }
 
-	// notification from PkgPool that a pkg has a new provides
+	/* notification from PkgPool that a pkg has a new provides
+	 <b>obsolete</b>
+	 */
 	void new_provides( PMSolvablePtr pkg, const PkgRelation& prov );
 
-	// look up a package by name
+	/** look up a package by name
+	 *
+	 * @return NULL if package is not in set
+	 * */
 	PMSolvablePtr lookup( const PkgName& name ) const {
 		const HashElt<PkgName,PMSolvablePtr > *p = contents.find(name);
 		return p ? p->value : NULL;
 	}
 	PMSolvablePtr operator[] ( const PkgName& name ) const { return lookup(name); }
-	// package contained in this set?
+	/**
+	 * package contained in this set?
+	 * */
 	bool includes( const PkgName& name ) const {
 		return contents.exists(name);
 	}
@@ -82,7 +102,24 @@ class PkgSet {
 	const_iterator begin() const { return contents.begin(); }
 	const_iterator end() const { return contents.end(); }
 
-	// member access methods
+	/**
+	 * The required, conflicted, provided and obsoleted methods return the
+	 * reverse indices for packages relations. The returned value is a hash
+	 * that can be indexed by a PkgName. For example, to learn the names of
+	 * all packages that require foo:
+	 * <code>
+	 * <pre>
+	 * PkgSet pset;
+	 * // ...
+	 * const PkgSet::RevRelList_type& requires_foo = pset.required()[PkgName("foo")];
+	 * for( PkgSet::RevRelList_const_iterator p = requires_foo.begin();
+	 *    p != requires_foo.end(); ++p ) {
+	 * cout << *p->pkg()->name << endl;
+	 * }
+	 * </pre>
+	 * </code>
+	 * */
+
 	const InvRel_type& required() const { return _required; }
 	const InvRel_type& conflicted() const { return _conflicted; }
 	const InvRel_type& provided() const { return _provided; }
