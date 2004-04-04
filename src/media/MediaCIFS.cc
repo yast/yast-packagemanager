ff81c6d870ad158140767f8b57ab0377801c235a
Klaus Kämpf <kkaempf@suse.de>
2004-04-04 12:58:04 +0000

    add "CIFS" support to media (#37871)
    svn path=/trunk/packagemanager/; revision=17482

---
+++ src/media/MediaCIFS.cc
@@ -0,0 +1,160 @@
+/*---------------------------------------------------------------------\
+|                                                                      |
+|                      __   __    ____ _____ ____                      |
+|                      \ \ / /_ _/ ___|_   _|___ \                     |
+|                       \ V / _` \___ \ | |   __) |                    |
+|                        | | (_| |___) || |  / __/                     |
+|                        |_|\__,_|____/ |_| |_____|                    |
+|                                                                      |
+|                               core system                            |
+|                                                        (C) SuSE GmbH |
+\----------------------------------------------------------------------/
+
+   File:	MediaCIFS.cc
+
+   Author:	Klaus Kaempf <kkaempf@suse.de>
+   Maintainer:	Klaus Kaempf <kkaempf@suse.de>
+
+   Purpose:	Implementation class for MediaHandler
+		This class handles access to CIFS media
+/-*/
+
+#include <iostream>
+
+#include <y2util/Y2SLog.h>
+#include <y2pm/Mount.h>
+#include <y2pm/MediaCIFS.h>
+
+#include <sys/types.h>
+#include <sys/mount.h>
+#include <errno.h>
+#include <dirent.h>
+
+using namespace std;
+
+///////////////////////////////////////////////////////////////////
+//
+//	CLASS NAME : MediaCIFS
+//
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : MediaCIFS::MediaCIFS
+//	METHOD TYPE : Constructor
+//
+//	DESCRIPTION :
+//
+MediaCIFS::MediaCIFS( const Url &      url_r,
+		    const Pathname & attach_point_hint_r )
+    : MediaHandler( url_r, attach_point_hint_r,
+		    false, // attachPoint_is_mediaroot
+		    false ) // does_download
+{
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : MediaCIFS::attachTo
+//	METHOD TYPE : PMError
+//
+//	DESCRIPTION : Asserted that not already attached, and attachPoint is a directory.
+//
+PMError MediaCIFS::attachTo(bool next)
+{
+    if(_url.host().empty())
+	    return Error::E_no_host_specified;
+    if(next)
+	return Error::E_not_supported_by_media;
+
+    const char* const filesystem = "cifs";
+    const char *mountpoint = attachPoint().asString().c_str();
+    Mount mount;
+    PMError ret;
+
+    string path = "//";
+    path += _url.host();
+    path += _url.path();
+
+    string options = _url.option("mountoptions");
+    string username = _url.username();
+    string password = _url.password();
+    // need to add guest to prevent smbmount from asking for password
+    if(options.empty())
+    {
+	options="ro,guest";
+    }
+    else if ( password.empty()
+	    && options.find("guest") == string::npos
+	    && options.find("credentials") == string::npos
+	    && options.find("password") == string::npos )
+    {
+	options += ",guest";
+    }
+
+    if(!username.empty())
+	options += ",user=" + username;
+
+    if(!password.empty())
+	options += ",password=" + password;
+
+    ret = mount.mount(path,mountpoint,filesystem,options);
+    if(ret != Error::E_ok)
+    {
+	return ret;
+    }
+
+    return Error::E_ok;
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : MediaCIFS::releaseFrom
+//	METHOD TYPE : PMError
+//
+//	DESCRIPTION : Asserted that media is attached.
+//
+PMError MediaCIFS::releaseFrom( bool eject )
+{
+    Mount mount;
+    PMError ret;
+
+    if ((ret = mount.umount(attachPoint().asString())) != Error::E_ok)
+    {
+	return ret;
+    }
+
+    return ret;
+}
+
+
+///////////////////////////////////////////////////////////////////
+//
+//	METHOD NAME : MediaCIFS::getFile
+//	METHOD TYPE : PMError
+//
+//	DESCRIPTION : Asserted that media is attached.
+//
+PMError MediaCIFS::getFile (const Pathname & filename) const
+{
+  return MediaHandler::getFile( filename );
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//
+//	METHOD NAME : MediaCIFS::getDirInfo
+//	METHOD TYPE : PMError
+//
+//	DESCRIPTION : Asserted that media is attached and retlist is empty.
+//
+PMError MediaCIFS::getDirInfo( std::list<std::string> & retlist,
+			      const Pathname & dirname, bool dots ) const
+{
+  return MediaHandler::getDirInfo( retlist, dirname, dots );
+}
+
+
