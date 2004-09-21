fa9dd1bd51de1085b610b95c1c0c1529af481654
Michael Andres <ma@suse.de>
2004-09-21 19:26:27 +0000

    Allow setting environment variables for 'mount' (e.g. PASSWD)
    svn path=/trunk/packagemanager/; revision=19522

--- src/y2pm/Mount.h
+++ src/y2pm/Mount.h
@@ -41,6 +41,12 @@ class Mount
 	 **/
         typedef MediaError Error;
 
+	/**
+	 * For passing additional environment variables
+	 * to mount
+	 **/
+	typedef ExternalProgram::Environment Environment;
+
     public:
 
 	/**
@@ -60,12 +66,14 @@ class Mount
 	* @param target where to mount (e.g. /mnt)
 	* @param filesystem which filesystem to use (e.g. reiserfs) (-t parameter)
 	* @param options mount options (e.g. ro) (-o parameter)
+	* @param environment optinal environment to pass (e.g. PASSWD="sennah")
 	*/
 
-	PMError mount (const std::string& source,
-		  const std::string& target,
-		  const std::string& filesystem,
-		  const std::string& options);
+	PMError mount ( const std::string& source,
+			const std::string& target,
+			const std::string& filesystem,
+			const std::string& options,
+			const Environment& environment = Environment() );
 
 	/** umount device
 	 *
@@ -81,13 +89,20 @@ class Mount
 
 	/**
 	 * Run mount with the specified arguments and handle stderr.
-	 * @param n_opts The number of arguments
-	 * @param options Array of the arguments, @ref n_opts elements
+	 * @param argv Mount arguments
+	 * @param environment Addittional environment to set
 	 * @param stderr_disp How to handle stderr, merged with stdout by default
 	 * */
-	void run(const char *const *argv,
-			  ExternalProgram::Stderr_Disposition stderr_disp =
-			  ExternalProgram::Stderr_To_Stdout);
+	void run( const char *const *argv, const Environment& environment,
+		  ExternalProgram::Stderr_Disposition stderr_disp =
+		  ExternalProgram::Stderr_To_Stdout);
+
+	void run( const char *const *argv,
+		  ExternalProgram::Stderr_Disposition stderr_disp =
+		  ExternalProgram::Stderr_To_Stdout) {
+	  Environment notused;
+	  run( argv, notused, stderr_disp );
+	}
 
 	/** Return the exit status of the process, closing the connection if
 	 * not already done.
