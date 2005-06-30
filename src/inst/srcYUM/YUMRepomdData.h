b06a97787a5497c1e0b546faf1e376f9034e646b
Michael Radziej <mir@suse.de>
2005-06-30 09:56:31 +0000

    initial stuff for the repomd parser. Not complete, probably does not compile.
    svn path=/trunk/packagemanager/; revision=23843

---
+++ src/inst/srcYUM/YUMRepomdData.h
@@ -0,0 +1,19 @@
+#ifndef YUMRepomdData_h
+#define YUMRepomdData_h
+
+#include <string>
+
+class YUMRepomdData : public CountedRep {
+  REP_BODY(YUMRepomdData);
+
+public:
+  std::string _type;
+  std::string _location;
+  std::string _checksumType;
+  std::string _checksum;
+  std::string _timestamp;
+  std::string _openChecksumType;
+  std::string _openChecksum;
+};
+
+#endif
---
+++ src/inst/srcYUM/YUMRepomdDataPtr.h
@@ -0,0 +1,8 @@
+#ifndef YUMRepomdDataPtr_h
+#define YUMRepomdDataPtr_h
+
+#include <y2util/RepDef.h>
+
+DEFINE_BASE_POINTER(YUMRepomdData);
+
+#endif 
