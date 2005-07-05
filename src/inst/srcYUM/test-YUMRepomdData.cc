7a6e4aa410a895a8e635ad2b1cff98c518351f7b
Michael Radziej <mir@suse.de>
2005-07-05 13:23:03 +0000

    Various fixes in XMLNodeIterator.h, learned that I need YUMRepomdDataPtr.cc, moved parser from YUMPrimaryData.cc to YUMPrimaryDataIterator.cc created a test (which immediately aborts)
    svn path=/trunk/packagemanager/; revision=23895

---
+++ src/inst/srcYUM/test-YUMRepomdData.cc
@@ -0,0 +1,30 @@
+#include "YUMRepomdDataIterator.h"
+#include "YUMRepomdDataPtr.h"
+#include "YUMRepomdData.h"
+
+using namespace std;
+
+void debugPrint(const YUMRepomdDataPtr data)
+{
+  cout << "Repomd Data: " << endl
+       << "  type: " << data->type << endl
+       << "  location: " << data->location << endl
+       << "  checksumType: " << data->checksumType << endl
+       << "  checksum: " << data->checksum << endl
+       << "  timestamp: " << data->timestamp << endl
+       << "  openChecksumType: " << data->openChecksumType << endl
+       << "  openChecksum: " << data->openChecksum << endl;
+}
+
+
+int main()
+{
+  for (YUMRepomdDataIterator iter(cin,"");
+       !iter.atEnd();
+       ++iter) {
+    debugPrint(*iter());
+  }
+  return 0;
+}
+         
+
