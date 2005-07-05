b45183c0a7599012e4d34276965dc13275035812
Michael Radziej <mir@suse.de>
2005-07-05 15:49:56 +0000

    progress parsing primary.xml; does not compile yet
    svn path=/trunk/packagemanager/; revision=23903

---
+++ src/inst/srcYUM/YUMPrimaryData.cc
@@ -0,0 +1,4 @@
+#include "YUMPrimaryData.h"
+
+YUMPrimaryData::YUMPrimaryData()
+{ }
---
+++ src/inst/srcYUM/YUMPrimaryDataPtr.h
@@ -0,0 +1,8 @@
+#ifndef YUMPrimaryDataPtr_h
+#define YUMPrimaryDataPtr_h
+
+#include <y2util/RepDef.h>
+
+DEFINE_BASE_POINTER(YUMPrimaryData);
+
+#endif
