55e40d1c1ca7409b5368a47bccbfcfda7272126e
Michael Radziej <mir@suse.de>
2005-07-05 16:43:41 +0000

    progress on primary parser, compiles now. Untested.
    svn path=/trunk/packagemanager/; revision=23905

--- src/inst/srcYUM/YUMPrimaryData.cc
+++ src/inst/srcYUM/YUMPrimaryData.cc
@@ -1,4 +1,12 @@
 #include "YUMPrimaryData.h"
 
+using namespace std;
+
 YUMPrimaryData::YUMPrimaryData()
 { }
+
+YUMPrimaryData::FileData::FileData(const string& name,
+                                   const string& type)
+  : name(name), type(type)
+{ }
+
