9c4add3b0fd511b92c092412003c8778cb90610e
Michael Radziej <mir@suse.de>
2005-07-18 09:21:36 +0000

    updated documentation about recent change in YUM parsing
    svn path=/trunk/packagemanager/; revision=24102

--- src/inst/srcYUM/YUMGroupParser.h
+++ src/inst/srcYUM/YUMGroupParser.h
@@ -10,6 +10,28 @@
 
 namespace YUM {
 
+  /**
+  *
+  * @short Parser for YUM group files.
+  *
+  * Use this class as an iterator that produces, one after one,
+  * YUMGroupDataPtr(s) for the XML group elements.
+  * Here's an example:
+  *
+  * for (YUMGroupParser iter(anIstream, baseUrl),
+  *      iter != YUMFileListParser.end(),     // or: iter() != 0, or ! iter.atEnd()
+  *      ++iter) {
+  *    doSomething(*iter)
+  * }
+  *
+  * The iterator owns the pointer (i.e., caller must not delete it)
+  * until the next ++ operator is called. At this time, it will be
+  * destroyed (and a new ENTRYTYPE is created.)
+  *
+  * If the input is fundamentally flawed so that it makes no sense to
+  * continue parsing, XMLNodeIterator will log it and consider the input as finished.
+  * You can query the exit status with errorStatus().
+  */
   class YUMGroupParser : public XMLNodeIterator<YUMGroupDataPtr>
   {
   public:
--- src/inst/srcYUM/YUMPrimaryParser.h
+++ src/inst/srcYUM/YUMPrimaryParser.h
@@ -10,8 +10,26 @@
 
 namespace YUM {
 
-  class YUMDependency;
-
+  /**
+  * @short Parser for YUM primary.xml files (containing package metadata)
+  * Use this class as an iterator that produces, one after one,
+  * YUMPrimaryDataPtr(s) for the XML package elements in the input.
+  * Here's an example:
+  *
+  * for (YUMPrimaryParser iter(anIstream, baseUrl),
+  *      iter != YUMOtherParser.end(),     // or: iter() != 0, or ! iter.atEnd()
+  *      ++iter) {
+  *    doSomething(*iter)
+  * }
+  *
+  * The iterator owns the pointer (i.e., caller must not delete it)
+  * until the next ++ operator is called. At this time, it will be
+  * destroyed (and a new ENTRYTYPE is created.)
+  *
+  * If the input is fundamentally flawed so that it makes no sense to
+  * continue parsing, XMLNodeIterator will log it and consider the input as finished.
+  * You can query the exit status with errorStatus().
+  */
   class YUMPrimaryParser : public XMLNodeIterator<YUMPrimaryDataPtr>
   {
   public:
