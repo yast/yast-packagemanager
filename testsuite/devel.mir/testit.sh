1bc9360d454ad41846bd1cde736eab7b3fe1aecf
Michael Radziej <mir@suse.de>
2005-07-28 18:25:25 +0000

    Finally figured out how to handle dejaGnu.
    svn path=/trunk/packagemanager/; revision=24463

--- testsuite/devel.mir/testit.sh
+++ testsuite/devel.mir/testit.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 # goes through each testcase in this directory
 # see README
-for testcase in *.test.xml
+for testcase in $1/*.test.xml
 do
+    # echo "$testcase" > testit.log
     mode="${testcase#*-}"
     mode="${mode%%-*}"
     ref="$testcase.ref"
     err_ref="$testcase.err.ref"
 
-    ../YUMtest "$mode" < "$testcase" > "$testcase.out" 2>"$testcase.err.tmp"
+    ./YUMtest "$mode" < "$testcase" > "$testcase.out" 2>"$testcase.err.tmp"
     echo "Exit Code: $?" >> "$testcase.out"
 
     # remove date and host name from logs
-    cut -b 36- < "$testcase.err.tmp" > "$testcase.err.out"
+    cut -d \  -f 5- < "$testcase.err.tmp" > "$testcase.err.out"
     rm "$testcase.err.tmp"
 
     if diff -q "$ref" "$testcase.out" > /dev/null \
        && diff -q "$err_ref" "$testcase.err.out" > /dev/null
     then
-        echo "pass $testcase"
+        echo "OK: $testcase"
     else
-        echo "fail $testcase"
+        echo "FAILED: $testcase"
+        exit 1
     fi
 done
 
