85355432d4ff4dc7a3950bf9eb7c9730cac660df
Michael Radziej <mir@suse.de>
2005-07-28 15:08:11 +0000

    still building testsuite
    svn path=/trunk/packagemanager/; revision=24458

--- testsuite/devel.mir/tests.YUMParser/testit.sh
+++ testsuite/devel.mir/tests.YUMParser/testit.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-for testcase in '*.test.xml'
+# goes through each testcase in this directory
+# see README
+for testcase in *.test.xml
 do
     mode="${testcase#*-}"
-    mode="${testcase%%-*}"
+    mode="${mode%%-*}"
     ref="$testcase.ref"
-    ../YUMtest "$mode" "$testcase" > "$testcase.out"
-    echo "Exit Code: $?" >> "$testcode.out"
-    if diff -q "$ref" "$testcase.out" > /dev/null
+    err_ref="$testcase.err.ref"
+
+    ../YUMtest "$mode" < "$testcase" > "$testcase.out" 2>"$testcase.err.tmp"
+    echo "Exit Code: $?" >> "$testcase.out"
+
+    # remove date and host name from logs
+    cut -b 36- < "$testcase.err.tmp" > "$testcase.err.out"
+    rm "$testcase.err.tmp"
+
+    if diff -q "$ref" "$testcase.out" > /dev/null \
+       && diff -q "$err_ref" "$testcase.err.out" > /dev/null
     then
         echo "pass $testcase"
     else
         echo "fail $testcase"
     fi
 done
+
 
